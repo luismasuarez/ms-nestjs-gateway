6554ef228c20a3424609f5bf9c322d6b7aa38503
Luisma Suarez <luisma.suarezzz@gmail.com>
2026-02-21 00:26:41 -0500

    feat: add Swagger integration and Scalar API reference to main application

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  // Validación global
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,13 +17,29 @@ async function bootstrap() {
     }),
   );
 
-  // CORS
   app.enableCors();
 
-  // Prefijo global
-  app.setGlobalPrefix('api');
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mi API')
+    .setDescription('Documentación de mi API')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 👇 Scalar bajo el mismo prefijo
+  app.use(
+    `/${globalPrefix}/reference`,
+    apiReference({
+      content: document,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`API Gateway running on http://localhost:${process.env.PORT ?? 3000}`);
+  logger.log(`Scalar docs on http://localhost:${process.env.PORT ?? 3000}/${globalPrefix}/reference`);
 }
 bootstrap();
